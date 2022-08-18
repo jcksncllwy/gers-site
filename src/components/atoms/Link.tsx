45941bef081340cb8217ab4cdcd447a3be151bdd
Jackson Callaway <jcksncllwy@gmail.com>
2022-08-17 20:03:13 -0700

    replacing things with MUI

---
+++ src/components/atoms/Link.tsx
@@ -0,0 +1,21 @@
+import { Link as RouterLink, LinkProps } from "react-router-dom";
+import styled from "styled-components";
+
+type Props = {
+    children: React.ReactNode;
+} & LinkProps;
+
+const Link = ({children, ...props}: Props)=>{
+    return(
+        <StyledLink {...props}>{children}</StyledLink>
+    )
+}
+
+const StyledLink = styled(RouterLink)`
+    text-decoration: none;
+    &:visited{
+        color: inherit;
+    }
+`
+
+export default Link;
--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,10 +1,15 @@
-import BasicPage from "../components/layouts/BasicPage";
+import { Box } from "@mui/material";
+import { Container } from "@mui/system";
+import { AppBar } from '../components/molecules/AppBar';
 
 const Splash = ()=>{
     return (
-        <BasicPage>
-            <img src="https://ltfowyvtpuhuazsxpcvn.supabase.co/storage/v1/object/public/public-images/GERS_Poster_11x17_2022_low_res.jpeg" />
-        </BasicPage>
+        <Box>
+            <AppBar />
+            <Container maxWidth="md" sx={{paddingTop: '40px'}}>
+                <img src="https://ltfowyvtpuhuazsxpcvn.supabase.co/storage/v1/object/public/public-images/GERS_Poster_11x17_2022_low_res.jpeg" />
+            </Container>
+        </Box>
     )
 }
 
