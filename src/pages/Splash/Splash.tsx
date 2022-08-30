4db2df08fe37e6f97574ab8185585c9793a30e9e
Jackson Callaway <jcksncllwy@gmail.com>
2022-08-30 15:33:59 -0700

    new features section on splash page

--- src/pages/Splash/Splash.tsx
+++ src/pages/Splash/Splash.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, ThemeProvider, Typography } from "@mui/material";
+import { Box, Button, Container, Paper, Typography } from "@mui/material";
 import Link from '../../components/atoms/Link';
 import Grid2 from "@mui/material/Unstable_Grid2";
 import React from "react";
@@ -77,7 +77,7 @@ const Banner = () => {
     )
 }
 
-const marqueeItems = ["Interactive Sessions", "Livestream restoration projects from around the globe", "Live Q&A", "Engaging workshops & applicable sessions", "Breakout groups for focused discussion", "Learn from world leaders in ecosystem restoration"]
+
 
 
 /**
@@ -85,59 +85,94 @@ const marqueeItems = ["Interactive Sessions", "Livestream restoration projects f
  * Convening restoration practitioners to network, share practical information, collaborate, celebrate our work, scale up local action, and build the global movement.
  */
 
+const About = () => {
+    return (
+        <Box sx={{ color: "#3187DE" }}>
+            <Grid2 container spacing={2}>
+                <Grid2 xs={12} sm={6} sx={{
+                    backgroundImage: 'url("https://ltfowyvtpuhuazsxpcvn.supabase.co/storage/v1/object/public/public-images/newforestfarm.jpeg")',
+                    backgroundPosition: "center",
+                    minHeight: "400px"
+                }}>
+                </Grid2>
+                <Grid2 xs={12} sm={6} sx={{ pr: 10, pl: 6, py: 6 }}>
+                    <Typography variant="h6" sx={{ mb: 4 }}>About the Event</Typography>
+                    <Typography fontSize="1.75rem" lineHeight="1.75rem" fontWeight="400" sx={{ mb: 3 }}>
+                        BRINGING TOGETHER & SERVING THE GLOBAL GRASSROOTS ECO-RESTORATION MOVEMENT
+                    </Typography>
+                    <Typography sx={{ color: "#000", mb: 2 }} variant="h6" fontWeight="700" >
+                        We want to restore ecosystems. And you're invited to join us.
+                    </Typography>
+                    <Typography sx={{ color: "#000", mb: 2 }}>
+                        <Typography display="inline" sx={{ fontWeight: 700 }}>October 21-22</Typography> will be dedicated to keynote speakers, workshops and small group sessions. All presentations and workshops will take place on Zoom.
+                    </Typography>
+                    <Typography sx={{ color: "#000", mb: 2 }}>
+                        <Typography display="inline" sx={{ fontWeight: 700 }}>October 23</Typography> in-person actions will be taking place around the globe. We will be broadcasting livestreams of these in-person events for you to watch along. Or, you can start your own!
+                    </Typography>
+                    <Typography sx={{ color: "#000", mb: 2 }}>
+                        <Typography display="inline" sx={{ fontWeight: 700 }}>October 24</Typography> is dedicated to crafting an International Plan of Action to present to the world.
+                    </Typography>
+                </Grid2>
+            </Grid2>
+        </Box>
+    )
+}
+
+const features = ["Live Q&A", "Interactive Sessions", "Livestream restoration projects from around the globe", "Engaging workshops & applicable sessions", "Breakout groups for focused discussion", "Learn from world leaders in ecosystem restoration"]
+type FeatureProps = { children: React.ReactNode }
+const FeatureItem = ({ children }: FeatureProps) => {
+    return (
+        <Typography
+            variant="h5"
+            fontWeight="300"
+            sx={{ mb: 2 }}
+        >
+            {children}
+        </Typography>
+    )
+}
+
+const Features = () => {
+    return (
+        <Box sx={{
+            backgroundColor: "rgb(0,30,60)",
+            color: "#fff",
+            py: 10,
+            px: 10
+        }}>
+            <Paper sx={{ p: 4 }}>
+                <Grid2 container spacing={4}>
+                    <Grid2 xs={12} md={7} sx={{pr:4}}>
+                        <ul>
+                            {
+                                features.map((feature) => {
+                                    return (
+                                        <li><FeatureItem>{feature}</FeatureItem></li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </Grid2>
+                    <Grid2 xs={12} md={5} sx={{
+                        backgroundImage: "url('https://globalearthrepairfoundation.org/wp-content/uploads/2022/03/navivillage-galeria-02a.jpg')",
+                        backgroundPosition: 'center',
+                        minHeight: "400px",
+                        borderRadius: "3px"
+                    }}>
+                    </Grid2>
+                </Grid2>
+            </Paper>
+        </Box>
+    )
+}
+
 const Splash = () => {
     return (
         <BannerPage banner={<Banner />} sx={{
             backgroundColor: "#fff",
         }}>
-            <Box sx={{ color: "#3187DE" }}>
-                <Grid2 container spacing={2}>
-                    <Grid2 xs={12} sm={6} sx={{
-                        backgroundImage: 'url("https://ltfowyvtpuhuazsxpcvn.supabase.co/storage/v1/object/public/public-images/newforestfarm.jpeg")'
-                    }}>
-                    </Grid2>
-                    <Grid2 xs={12} sm={6} sx={{ pr: 10, pl: 6, py: 4 }}>
-                        <Typography variant="h6" sx={{ mb: 4 }}>About the Event</Typography>
-                        <Typography fontSize="1.75rem" lineHeight="1.75rem" fontWeight="400" sx={{ mb: 3 }}>
-                            BRINGING TOGETHER & SERVING THE GLOBAL GRASSROOTS ECO-RESTORATION MOVEMENT
-                        </Typography>
-                        <Typography sx={{ color: "#000", mb: 2 }} variant="h6" fontWeight="700" >
-                            We want to restore ecosystems. And you're invited to join us.
-                        </Typography>
-                        <Typography sx={{ color: "#000", mb: 2 }}>
-                            <Typography display="inline" sx={{ fontWeight: 700 }}>October 21-22</Typography> will be dedicated to keynote speakers, workshops and small group sessions. All presentations and workshops will take place on Zoom.
-                        </Typography>
-                        <Typography sx={{ color: "#000", mb: 2 }}>
-                            <Typography display="inline" sx={{ fontWeight: 700 }}>October 23</Typography> in-person actions will be taking place around the globe. We will be broadcasting livestreams of these in-person events for you to watch along. Or, you can start your own!
-                        </Typography>
-                        <Typography sx={{ color: "#000", mb: 2 }}>
-                            <Typography display="inline" sx={{ fontWeight: 700 }}>October 24</Typography> is dedicated to crafting an International Plan of Action to present to the world.
-                        </Typography>
-                    </Grid2>
-                </Grid2>
-            </Box>
-            <Box sx={{
-                backgroundColor: "rgb(0,30,60)",
-                color: "#fff",
-                py: 3
-            }}>
-                <Ticker>
-                    {() => (
-                        <>
-                            <Typography variant="h5" fontWeight="300" noWrap>
-                                {marqueeItems.map((item) => {
-                                    return (
-                                        <>
-                                            <Box display="inline" sx={{ px: 4 }}>{item}</Box>
-                                            <Box display="inline" sx={{ px: 4 }}>—</Box>
-                                        </>
-                                    )
-                                })}
-                            </Typography>
-                        </>
-                    )}
-                </Ticker>
-            </Box>
+            <About />
+            <Features />
             <Box sx={{ backgroundColor: "#3187DE", py: 10 }}>
                 <Container sx={{ pb: 4 }}>
                     <Typography
